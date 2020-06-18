a4648dafe905651325b10497af1e665ce6594fed
gintu tom <gintutom@gmail.com>
2020-06-18 23:37:20 +0530

    slight ui fix

--- src/components/create/create.js
+++ src/components/create/create.js
@@ -5,9 +5,16 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
 import Form from "../form";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  paper: { Width: "250px" }
+}));
 
 export default function Create({ muscles, handleCreate }) {
   const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+  console.log(classes);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -30,6 +37,7 @@ export default function Create({ muscles, handleCreate }) {
       <Dialog
         open={open}
         onClose={handleClose}
+        classes={{ paper: classes.paper }}
         aria-labelledby="form-dialog-title">
         <DialogTitle id="form-dialog-title">Create New Exercise</DialogTitle>
         <DialogContent>
